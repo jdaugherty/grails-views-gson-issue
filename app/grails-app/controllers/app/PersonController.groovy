faf770b71cc476b94234041048d99fc258df7560
James Daugherty <jdaugherty@trtn.com>
2020-09-11 12:41:24 -0400

    Upgrade to grails 4, switch app to rest plugin, and provide example

--- app/grails-app/controllers/app/PersonController.groovy
+++ app/grails-app/controllers/app/PersonController.groovy
@@ -1,13 +1,31 @@
 package app
 
 import demo.RandomPersonService
+import grails.validation.Validateable
 import groovy.transform.CompileStatic
+import org.springframework.http.HttpStatus
 
 @CompileStatic
 class PersonController {
     RandomPersonService randomPersonService
     def index() {
-        render randomPersonService.randomOciPersonName()
+        PersonCommand cmd = new PersonCommand()
+        cmd.name = randomPersonService.randomOciPersonName()
+        cmd.title = "Grand Architect"
+        respond cmd, [view: 'index', status: HttpStatus.OK]
+    }
+}
+
+class PersonCommand implements Validateable {
+    String name
+    String title
+
+    static constraints = {
+        name(nullable: false)
+    }
+
+    static boolean defaultNullable() {
+        true
     }
 }
 
